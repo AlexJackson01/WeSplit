9f1e3ebeff38d5e435cbe7a540fc8b788d941d8d
Alex Jackson <ajjackson22@gmail.com>
2025-02-22 19:18:08 +0000

    Tutorial changes

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,30 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    @State private var name = ""
-    let students = ["Alex", "Lee"]
-    @State private var selectedStudent = "Alex"
+    @State private var billAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = billAmount / 100 * tipSelection
+        let grandTotal = billAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
 
     var body: some View {
         NavigationStack {
 
             Form {
-                Picker("Select name", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                // Bill amount / number of people
+                Section {
+                    TextField(
+                        "Amount", value: $billAmount,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "GBP")
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
                     }
                 }
-                TextField("Name", text: $name)
-                Text("Hello, \(name)")
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+                // Tip amount
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                // Total per person
+                Section {
+                    Text(
+                        totalPerPerson,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "GBP"))
+                }
 
-            Button("Tap count: \(tapCount)") {
-                tapCount += 1
             }
-
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
